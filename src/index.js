4cf3a0123ac45c35b06ba3b4fefd7749601cbbbe
Collins Chirchir <collinskchirchir@gmail.com>
2022-12-01 21:21:29 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form');
+  form.addEventListener('submit', e => 
+    {
+      e.preventDefault()
+      textInput = document.querySelector("#new-task-description").value;
+      itemToDo(textInput)
+      form.reset()
+       
+     
+    })
+  });
+
+// Function for adding to do items
+function itemToDo(todo)
+{
+  let li = document.createElement('p');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo);
+  btn.textContent = "x"
+  li.textContent = `${todo}  `
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+
+}
+
+function deleteToDo(e)
+{
+  e.target.parentNode.remove()
+}
